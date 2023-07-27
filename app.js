262d482fdacfc54879419263e79d48a7d3630619
Wilson_Carrizo <wolfc777@gmail.com>
2023-07-27 15:31:30 -0300

    fix estructura +fformulario

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
+const express = require("express")
 
-const express=require ('express')// requerimos express
-const app=express()//lo guardamos en una variable llamada app ejecutado
-const path= require("path")//requerimos el modulo path para usar las funciones .join y hacer que nos concatene las rutas dandole las ubicaciones 
-const PORT=3030//creamos una constante con el valor del puerto 3030 que es el que vamos a escuchar
-app.use(express.static('public'))//usamos la funcion .static de express para usar archivos estaticos del servidor que esta en public (imagenes)
-app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')))// creamos la ruta de nuestro .html
-app.get('/ayuda',(req,res)=>res.sendFile(path.join(__dirname,'views','ayuda.html')))
-app.get('/creaTuCuenta',(req,res)=>res.sendFile(path.join(__dirname,'views','creaTuCuenta.html')))
-app.get('/ingresa',(req,res)=>res.sendFile(path.join(__dirname,'views','ingresa.html')))
-app.get('/MisCompras',(req,res)=>res.sendFile(path.join(__dirname,'views','MisCompras.html')))
-app.get('/ofertas',(req,res)=>res.sendFile(path.join(__dirname,'views','ofertas.html')))
-app.get('/tiendasOficiales',(req,res)=>res.sendFile(path.join(__dirname,'views','tiendasOficiales.html')))
-app.get('/vender',(req,res)=>res.sendFile(path.join(__dirname,'views','vender.html')))
-app.get('/*',(req,res)=>res.sendFile(path.join(__dirname,'views','404.html')))
-app.listen(PORT,()=>console.log("MercadoLiebre ON "+PORT))// usamos listen para escuchar y levantar nuestro servidor
+const app = express()
 
+const path = require("path")
+
+const PORT = 3030
+
+app.use(express.static("public"))
+
+app.listen(PORT, () => console.log(`servidor corriendo en http://localhost:`+PORT))
+
+app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","home.html"))) 
+
+app.get("/home",(req,res) => res.sendFile(path.join(__dirname,"views","home.html"))) 
+
+app.get("/register",(req,res) => res.sendFile(path.join(__dirname,"views","register.html")))
+
+app.get("/login",(req,res) => res.sendFile(path.join(__dirname,"views","login.html")))
