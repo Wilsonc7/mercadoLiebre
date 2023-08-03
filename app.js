1b43f520cbf7f757cf074933a739b4b3660659c1
Wilsonc7 <wolfc777@gmail.com>
2023-08-03 13:08:02 -0300

    animaciones edit

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
+/* //////////////////////////////////////////////////////////////////////////////Requires ////////////////////////////////////////////////////////////////////////////////////////////////*/
 const express = require("express")
-
 const app = express()
-
 const path = require("path")
-
 const PORT = 3030
 
 app.use(express.static("public"))
-
 app.listen(PORT, () => console.log(`servidor corriendo en http://localhost:`+PORT))
+/* ////////////////////////////////////////////////////////Direcciones //////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
 
 app.get("/",(req,res) => res.sendFile(path.join(__dirname,"views","home.html"))) 
 
@@ -17,3 +15,5 @@ app.get("/home",(req,res) => res.sendFile(path.join(__dirname,"views","home.html
 app.get("/register",(req,res) => res.sendFile(path.join(__dirname,"views","register.html")))
 
 app.get("/login",(req,res) => res.sendFile(path.join(__dirname,"views","login.html")))
+
+
